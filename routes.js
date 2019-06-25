049808c13afa70ae716c4d45c747fbccfe66a1ca
armin <armin.abbasi@hotmail.com>
2019-06-25 12:41:40 +0430

    added new sample list route

--- routes.js
+++ routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const models = require('./models');
 
 router.get('/', (req, res) => {
-    res.send('main route');
+    models.User.findAll({include: [{model: models.Poll}]}).then(users => res.send(users));
 });
 
 module.exports = router;
