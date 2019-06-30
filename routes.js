4e8ddd5eff3ba53bd01fb57090cc3b12496cb71b
armin <armin.abbasi@hotmail.com>
2019-06-30 16:15:34 +0430

    added response object

--- routes.js
+++ routes.js
@@ -1,14 +1,25 @@
 const router = require('express').Router();
 const pollService = require('./services/polls');
+let response = {code: 0, message: 'operation completed successfully.', data: []};
+
+const handleErrors = (err, response) => {
+    response.code = -1;
+    response.data = [];
+    response.message = (process.env.DEBUG === true) ? err.message : 'operation failed!';
+
+    return response;
+};
 
 router.get('/polls', (req, res) => {
     pollService
     .getAll()
-    .then(polls => {
-        res.send(polls);
+    .then(poll => {
+        response.data = polls;
+
+        return res.json(response);
     })
     .catch(err => {
-        console.log(err);
+      return res.json(handleErrors(err, response));
     });
 });
 
