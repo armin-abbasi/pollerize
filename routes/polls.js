f595c1e18f43f0a7c5b0c03c0e7573cb5a41b674
Armin Abbasi <armin.abbasi@hotmail.com>
2019-07-06 00:25:13 +0430

    pass req and res parameters to services

--- routes/polls.js
+++ routes/polls.js
@@ -1,26 +1,9 @@
 const router = require('express').Router();
 const pollService = require('../services/polls');
-let response = {code: 0, message: 'completed successfully', data: []};
-
-const handleErrors = (err, response) => {
-    response.code = -1;
-    response.data = [];
-    response.message = (process.env.DEBUG === true) ? err.message : 'operation failed!';
-
-    return response;
-};
 
 router.get('/', (req, res) => {
     pollService
-    .getAll()
-    .then(polls => {
-        response.data = polls;
-
-        return res.json(response);
-    })
-    .catch(err => {
-      return res.json(handleErrors(err, response));
-    });
+    .getAll(req, res);
 });
 
 module.exports = router;
--- services/polls.js
+++ services/polls.js
@@ -1,8 +1,26 @@
 const models = require('../models');
 const Poll = models.Poll;
 
-const getAll = () => {
-    return Poll.findAll();
+// Handling responses
+let response = {code: 0, message: 'completed successfully', data: []};
+const handleErrors = (err, response) => {
+    response.code = -1;
+    response.data = [];
+    response.message = (process.env.DEBUG === true) ? err.message : 'operation failed!';
+
+    return response;
+};
+
+const getAll = (req, res) => {
+    Poll
+        .findAll()
+        .then(polls => {
+            response.data = polls;
+            res.json(response).end();
+        })
+        .catch((err => {
+            handleErrors(err, response);
+        }));
 };
 
 module.exports = {getAll};
