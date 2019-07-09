a9be1c816c4f296881b0cc91ba2ac2182f2d5491
armin <armin.abbasi@hotmail.com>
2019-07-09 13:59:20 +0430

    created response module

--- utils/responser.js
+++ utils/responser.js
@@ -1,5 +1,34 @@
-const errorMessageMap = 
+const errorMessageMap = {
+    '0' : 'success',
+    '-1': 'failed',
+    '-2': 'not found',
+    '-3': 'unauthorized'
+};
 
-module.exports.create = (res, type, code) => {
+const errorStatusCode = {
+    '-2': 404,
+    '-3': 403
+};
+
+module.exports.create = (res, type, code, data) => {
+    // Get error message from error code.
+    let resMessage = errorMessageMap[code.toString()];
+
+    // Get error status code from error code.
+    let resStatus = errorStatusCode[code.toString()] || 200;
+
     
+    let resData = data;
+    
+    if (type === 'error') {
+        resData = (process.env.DEBUG === true) ? err.message : [];
+    }
+
+    let responseObject = {
+        'code': code,
+        'message': resMessage,
+        'data': resData
+    };
+
+    return res.status(resStatus).json(responseObject);
 };
