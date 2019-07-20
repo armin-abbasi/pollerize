4da3b141fd535d28ed76be823f2f40b4d4903ecf
Armin Abbasi <armin.abbasi@hotmail.com>
2019-07-20 23:25:34 +0430

    added users route

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const server = require('express')();
 const config = require('./config/app');
 const pollRoutes = require('./routes/polls');
+const userRoutes = require('./routes/users');
 const bodyParser = require('body-parser');
 const port = config.port || 3000;
 
 server.use(bodyParser.json({ type: 'application/json' }));
 
 server.use('/polls', pollRoutes);
+
+server.use('/users', userRoutes);
 
 server.listen(port, () => {
     console.log(`app listening to port ${port}`);
