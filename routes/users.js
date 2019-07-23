dc80d08b20c7371a0119ebcbb89f917a889cccaf
armin <armin.abbasi@hotmail.com>
2019-07-23 10:56:35 +0430

    added user service methods to users.js route file

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,24 @@
 const router = require('express').Router();
+const userService = require('../services/users');
+
+router.post('/', (req, res) => {
+    userService
+        .create(req, res);
+});
+
+router.get('/', (req, res) => {
+    userService
+        .getAll(req, res);
+});
+
+router.get('/:id', (req, res) => {
+    userService
+        .getById(req, res);
+});
+
+router.delete('/:id', (req, res) => {
+    userService
+        .deleteById(req, res);
+});
+
+module.exports = router;
