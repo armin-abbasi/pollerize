cbd673af40555eda98691415a7fe43d1892ad885
armin <armin.abbasi@hotmail.com>
2019-07-23 11:03:21 +0430

    prettified

--- utils/responser.js
+++ utils/responser.js
@@ -13,10 +13,9 @@ const errorStatusCode = {
 module.exports.create = (res, code, data) => {
     // Get error message from error code.
     let resMessage = errorMessageMap[code.toString()];
-
     // Get error status code from error code.
     let resStatus = errorStatusCode[code.toString()] || 200;
-
+    
     let resData = data;
     
     if (data !== null && data.hasOwnProperty('message') && code !== 0) {
