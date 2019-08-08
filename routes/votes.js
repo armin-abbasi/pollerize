c63385d77694bda5ec185b7064b2264e3bcd7820
Armin Abbasi <armin.abbasi@hotmail.com>
2019-08-08 23:39:37 +0430

    added votes create method

--- routes/votes.js
+++ routes/votes.js
@@ -4,8 +4,13 @@ const authMiddleware = require('../middlewares/authentication');
 
 router.use(authMiddleware.check);
 
-router.post('/:pollId/poll', (req, res) => {
+router.post('/:pollId/create', (req, res) => {
+    voteService
+        .create(req, res);
+});
 
+router.post('/:pollId/poll', (req, res) => {
+    
 });
 
 router.post('/:pollId/un-poll', (req, res) => {
--- services/votes.js
+++ services/votes.js
@@ -1,3 +1,32 @@
 const models = require('../models');
 const Vote = models.Vote;
 const Responser = require('../utils/responser');
+
+const create = (req, res) => {
+    let pollId = req.params.pollId;
+    let answer = req.body.answer;
+    let count = 0;
+    Vote
+        .create({
+            pollId,
+            answer,
+            count
+        })
+        .then((result) => {
+            return Responser.create(res, 0, result);
+        })
+        .catch((err) => {
+            return Responser.create(res, -1, err);
+        });
+
+};
+
+const poll = (req, res) => {
+
+};
+
+const unPoll = (req, res) => {
+
+};
+
+module.exports = {create, poll, unPoll};
