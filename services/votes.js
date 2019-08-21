8fb83cb385ad00f246d57e34f7bb53c0d0537b8d
armin <armin.abbasi@hotmail.com>
2019-08-21 14:25:03 +0430

    code clean up

--- services/votes.js
+++ services/votes.js
@@ -7,7 +7,6 @@ const create = (req, res) => {
     let pollId = req.body.pollId;
     let answer = req.body.answer;
     let count = 0;
-    
     Vote
         .create({
             pollId,
@@ -27,11 +26,9 @@ const deleteById = (req, res) => {
         .destroy({where: {id: req.params.id}})
         .then(result => {
             let responseCode = 0;
-
             if (result === 0) {
                 responseCode = -2;
             }
-
             return Responser.create(res, responseCode, []);
         })
         .catch(err => {
@@ -40,25 +37,18 @@ const deleteById = (req, res) => {
 };
 
 const poll = async(req, res) => {
+    let voteId = req.body.voteId;
+    let userId = req.body.user.id;
     try {
-        let voteId = req.body.voteId;
-        let userId = req.body.user.id;
-
         let PollVote = await Vote.findByPk(voteId);
-        
         let foundVotes = await UserVote.findAll({where: {voteId: PollVote.id, userId}});
-
         if (foundVotes.length !== 0) {
             return Responser.create(res, -1, {message: "You've voted before"});
         }
-
         // Add this vote's count value and update
         let count = ++PollVote.count;
-
         let updatedItem = await PollVote.update({count});
-
         let result = await UserVote.create({userId, voteId});
-
         // Return final result as success message
         return Responser.create(res, 0, result);
     } catch (err) {
@@ -66,37 +56,22 @@ const poll = async(req, res) => {
     }
 };
 
-const unPoll = (req, res) => {
+const unPoll = async(req, res) => {
     let voteId = req.body.voteId;
     let userId = req.body.user.id;
-
-    UserVote
-        .destroy({where: {voteId, userId}})
-        .then(result => {
-            // Decrease vote's count number
-            Vote
-                .findByPk(voteId)
-                .then(Vote => {
-                    let count = Vote.count > 0 ? --Vote.count : 0;
-
-                    Vote
-                        .update({count})
-                        .then(updatedItem => {
-                            return Responser.create(res, 0, updatedItem);
-                        })
-                        .catch(err => {
-                            return Responser.create(res, -1, err);
-                        });
-                })
-                .catch(err => {
-                    return Responser.create(res, -1, err);
-                });
-        })
-        .catch(err => {
-            return Responser.create(res, 1, err);
-        });
-
-    
+    try {
+        let deleteResult = await UserVote.destroy({where: {voteId, userId}});
+        if (deleteResult) {
+            let pollVote = await Vote.findByPk(voteId);     
+            // Increase vote count by one unit.
+            let count = pollVote.count > 0 ? --pollVote.count : 0;           
+            let updatedItem = await pollVote.update({count});
+            return Responser.create(res, 0, updatedItem);
+        }
+        return Responser.create(res, -1, []);
+    } catch (err) {
+        return Responser.create(res, -1, err);
+    }  
 };
 
 module.exports = {create, poll, unPoll, delete: deleteById};
