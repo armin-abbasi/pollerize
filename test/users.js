ed9b596c0afaa395c8de33084db10b54802f3f1b
Armin Abbasi <armin.abbasi@hotmail.com>
2019-08-31 15:00:48 +0430

    added create user api test

--- test/users.js
+++ test/users.js
@@ -1,7 +1,6 @@
 process.env.NODE_ENV = 'test';
 let app = require('../app');
 let chai = require('chai');
-let should = chai.should;
 let chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
@@ -9,10 +8,11 @@ chai.use(chaiHttp);
 // API tests for users service module.
 
 describe('Get users', () => {
-    it('it should return all of users', (done) => {
+    it('should return all of users', (done) => {
         chai.request(app)
             .get('/users')
             .end((err, res) => {
+                if (err) throw err;
                 chai.expect(res.status).to.be.equal(200);
                 chai.expect(res.body).to.be.a('object');
                 chai.expect(res.body.code).to.be.equal(0);
@@ -20,3 +20,28 @@ describe('Get users', () => {
           });
     });
 });
+
+describe('Create user', () => {
+    it('should create a user successfully', (done) => {
+        let user = {
+            "name": "John Doe",
+            "username": "john.doe",
+            "password": "abcd",
+            "gender": "male",
+            "dob": "1990-06-15",
+            "location": "iran/tehran",
+            "active": true
+        };
+
+        chai.request(app)
+            .post('/users')
+            .send(user)
+            .end((err, res) => {
+                if (err) throw err;
+                chai.expect(res.status).to.be.equal(200);
+                chai.expect(res.body.data.name).to.be.equal(user.name);
+                chai.expect(res.body.data.username).to.be.equal(user.username);
+                done();
+            });
+    });
+})
