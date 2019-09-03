4e34a2b1289e63a757a61cccb15e7fa028a1fcdc
Armin Abbasi <armin.abbasi@hotmail.com>
2019-09-03 23:23:48 +0430

    added polls integration test

---
+++ test/polls.js
@@ -0,0 +1,40 @@
+process.env.NODE_ENV = 'test';
+let app = require('../app');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let expect = chai.expect;
+
+chai.use(chaiHttp);
+
+// API tests for polls service module.
+
+getToken = (callback) => {
+    chai.request(app)
+        .post('/users/sign-in')
+        .send({
+            username: "test",
+            password: "abcd"
+        })
+        .end((err, res) => {
+            if (err) throw err;
+            callback(res.body.data.token);
+        }
+    );
+}
+
+describe('Get polls', () => {
+    it('should return all of polls', (done) => {
+        getToken((token) => {
+            chai.request(app)
+            .get('/polls')
+            .set('Authorization', `Bearer ${token}`)
+            .end((err, res) => {
+                if (err) throw err;
+                expect(res.status).to.be.equal(200);
+                expect(res.body).to.be.a('object');
+                expect(res.body.code).to.be.equal(0);
+                done();
+            });
+        });
+    });
+});
