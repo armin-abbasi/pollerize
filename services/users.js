01604a684b34ecaaa29baaacc26ad94d4019c0e5
Armin Abbasi <armin.abbasi@hotmail.com>
2019-09-25 11:48:47 +0330

    convert users service to class

--- services/users.js
+++ services/users.js
@@ -2,73 +2,79 @@ const models = require('../models');
 const User = models.User;
 const Responser = require('../utils/responser');
 
-const create = (req, res) => {
-    // Hash the entered password
-    let input = req.body;
-    input.password = User.generateHash(input.password);
+class Users {
 
-    User
-        .create(input)
-        .then(result => {
-            return Responser.create(res, 0, result);
-        })
-        .catch(err => {
-            return Responser.create(res, -1, err);
-        });
-};
+    static create(req, res) {
+        // Hash the entered password
+        let input = req.body;
+        input.password = User.generateHash(input.password);
 
-const getAll = (req, res) => {
-    User
-        .findAll()
-        .then(Users => {
-            return Responser.create(res, 0, Users);
-        })
-        .catch(err => {
-            return Responser.create(res, -1, err);
-        });
-};
+        User
+            .create(input)
+            .then(result => {
+                return Responser.create(res, 0, result);
+            })
+            .catch(err => {
+                return Responser.create(res, -1, err);
+            });
+    }
 
-const getById = (req, res) => {
-    User
-        .findByPk(req.params.id)
-        .then(User => {
-            return Responser.create(res, 0, User);
-        })
-        .catch(err => {
-            return Responser.create(res, -1, err);
-        });
-};
+    static getAll(req, res) {
+        User
+            .findAll()
+            .then(Users => {
+                return Responser.create(res, 0, Users);
+            })
+            .catch(err => {
+                return Responser.create(res, -1, err);
+            });
+    }
 
-const update = (req, res) => {
-    User
-        .findByPk(req.params.id)
-        .then(User => {
-            User.update(req.body)
-                .then(updatedItem => {
-                    return Responser.create(res, 0, updatedItem);
-                })
-                .catch(err => { return Responser.create(res, -1, err) });
-        })
-        .catch(err => {
-            return Responser.create(res, -1, err);
-        });
-};
+    static getById(req, res) {
+        User
+            .findByPk(req.params.id)
+            .then(User => {
+                return Responser.create(res, 0, User);
+            })
+            .catch(err => {
+                return Responser.create(res, -1, err);
+            });
+    }
 
-const deleteById = (req, res) => {
-    User
-        .destroy({where: {id: req.params.id}})
-        .then(result => {
-            let responseCode = 0;
+    static update(req, res) {
+        User
+            .findByPk(req.params.id)
+            .then(User => {
+                User.update(req.body)
+                    .then(updatedItem => {
+                        return Responser.create(res, 0, updatedItem);
+                    })
+                    .catch(err => {
+                        return Responser.create(res, -1, err)
+                    });
+            })
+            .catch(err => {
+                return Responser.create(res, -1, err);
+            });
+    }
 
-            if (result === 0) {
-                responseCode = -2;
-            }
+    static deleteById(req, res) {
+        User
+            .destroy({where: {id: req.params.id}})
+            .then(result => {
+                let responseCode = 0;
 
-            return Responser.create(res, responseCode, []);
-        })
-        .catch(err => {
-            return Responser.create(res, -1, err);
-        });
-};
+                if (result === 0) {
+                    responseCode = -2;
+                }
 
-module.exports = {getAll, getById, deleteById, create, update};
+                return Responser.create(res, responseCode, []);
+            })
+            .catch(err => {
+                return Responser.create(res, -1, err);
+            });
+    }
+
+}
+
+module.exports = Users;
